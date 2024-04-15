fa2b937b2470a8d89054e91898fc57c0b5243808
rubykasif <125839524+rubykasif@users.noreply.github.com>
2024-04-15 14:31:39 +1000

    Update simple_driving_env.py

--- simple_driving/envs/simple_driving_env.py
+++ simple_driving/envs/simple_driving_env.py
@@ -84,6 +84,7 @@ class SimpleDrivingEnv(gym.Env):
             #print("reached goal")
             self.done = True
             self.reached_goal = True
+            reward += 50
 
         ob = car_ob
         return ob, reward, self.done, dict()
